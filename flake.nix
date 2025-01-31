519de750eaccea12e45a2265050d7bc76f935b85
Pavel Sobolev <contact@paveloom.dev>
2025-01-31 23:07:59 +0300

    Add a Nix flake.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:paveloom/nixpkgs/system";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function (import nixpkgs { inherit system; })
+        );
+    in
+    {
+      devShells = forSystems (pkgs: {
+        default = pkgs.mkShell {
+          name = "eirene-direnv-shell";
+
+          buildInputs = with pkgs; [
+            bashInteractive
+            nixd
+            nixfmt-rfc-style
+          ];
+        };
+      });
+    };
+}
