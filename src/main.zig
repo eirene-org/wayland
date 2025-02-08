5f12e41b015e05d79ca5c3218bb0227138d8ed10
Pavel Sobolev <contact@paveloom.dev>
2025-02-08 17:57:42 +0300

    Obtain and print the path to a Wayland compositor's Unix socket.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,21 @@
 const std = @import("std");
 
+fn getSocketPath(allocator: std.mem.Allocator) ![]const u8 {
+    const xdg_runtime_dir = std.posix.getenv("XDG_RUNTIME_DIR") orelse "/run/user/1000";
+    const name = std.posix.getenv("WAYLAND_DISPLAY") orelse "wayland-0";
+
+    return std.fs.path.join(allocator, &.{ xdg_runtime_dir, name });
+}
+
 pub fn main() !void {
-    std.debug.print("Hello there!\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    const allocator = gpa.allocator();
+
+    const path = try getSocketPath(allocator);
+    defer allocator.free(path);
+
+    const stderr = std.io.getStdErr().writer();
+    stderr.print("wayland socket path: {s}\n", .{path}) catch {};
 }
