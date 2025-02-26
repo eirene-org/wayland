dd7bd3cce9e29ee79d64e965ef4eb0fa26ea9a10
Pavel Sobolev <contact@paveloom.dev>
2025-02-26 16:36:33 +0300

    Rework the build script. Create the examples implicitly.

--- build.zig
+++ build.zig
@@ -1,69 +1,126 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) !void {
-    const target = b.standardTargetOptions(.{});
-    const optimize = b.standardOptimizeOption(.{
-        .preferred_optimize_mode = .ReleaseFast,
-    });
+const BuildManagerOptions = struct {
+    target: std.Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    use_llvm: ?bool,
+    strip: ?bool,
+};
 
-    const is_debug_mode = optimize == .Debug;
+const BuildManagerModules = struct {
+    wayland_client: *std.Build.Module,
+    wayland_protocols: *std.Build.Module,
+};
 
-    const use_llvm = b.option(bool, "use_llvm", "Use the LLVM backend") orelse !is_debug_mode;
-    const strip = b.option(bool, "strip", "Strip debug symbols") orelse !is_debug_mode;
+const BuildManagerSteps = struct {
+    check: *std.Build.Step,
+    run: *std.Build.Step,
+};
 
-    const module_wayland_protocols = b.addModule("wayland-protocols", .{
-        .root_source_file = b.path("src/protocols/root.zig"),
-    });
+const BuildManager = struct {
+    b: *std.Build,
 
-    const module_wayland_client = b.addModule("wayland-client", .{
-        .root_source_file = b.path("src/client/root.zig"),
-        .imports = &.{
-            .{ .name = "wayland-protocols", .module = module_wayland_protocols },
-        },
-    });
+    options: BuildManagerOptions,
+    modules: BuildManagerModules,
+    steps: BuildManagerSteps,
 
-    const example_globals = b.addExecutable(.{
-        .name = "globals",
-        .root_source_file = b.path("examples/globals.zig"),
-        .target = target,
-        .optimize = optimize,
-        .use_lld = use_llvm,
-        .use_llvm = use_llvm,
-        .strip = strip,
-    });
-    example_globals.root_module.addImport("wayland-client", module_wayland_client);
-    example_globals.root_module.addImport("wayland-protocols", module_wayland_protocols);
+    const Self = @This();
 
-    const check_step = b.step("check", "Check if examples compile");
-    check_step.dependOn(&example_globals.step);
+    fn initOptions(b: *std.Build) BuildManagerOptions {
+        const target = b.standardTargetOptions(.{});
+        const optimize = b.standardOptimizeOption(.{
+            .preferred_optimize_mode = .ReleaseFast,
+        });
 
-    var examples = std.StringHashMap(*std.Build.Step.Compile).init(b.allocator);
-    defer examples.deinit();
+        const is_debug_mode = optimize == .Debug;
 
-    try examples.put("globals", example_globals);
+        const use_llvm = b.option(bool, "use_llvm", "Use the LLVM backend") orelse !is_debug_mode;
+        const strip = b.option(bool, "strip", "Strip debug symbols") orelse !is_debug_mode;
 
-    const example_name_option = b.option([]const u8, "example", "The name of the example to run");
-
-    const run_step = b.step("run", "Run an example (set via `-Dexample`)");
-
-    if (example_name_option) |example_name| blk: {
-        const example = examples.get(example_name) orelse {
-            const stderr = std.io.getStdErr().writer();
-            stderr.print("No such example.\n\n", .{}) catch {};
-
-            stderr.print("Available examples:\n", .{}) catch {};
-            var available_example_names = examples.keyIterator();
-            while (available_example_names.next()) |available_example_name| {
-                stderr.print("- {s}\n", .{available_example_name.*}) catch {};
-            }
-
-            break :blk;
+        return .{
+            .target = target,
+            .optimize = optimize,
+            .use_llvm = use_llvm,
+            .strip = strip,
         };
+    }
 
-        const run_cmd = b.addRunArtifact(example);
-        run_step.dependOn(&run_cmd.step);
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
+    fn initModules(b: *std.Build) BuildManagerModules {
+        const wayland_protocols = b.addModule("wayland-protocols", .{
+            .root_source_file = b.path("src/protocols/root.zig"),
+        });
+
+        const wayland_client = b.addModule("wayland-client", .{
+            .root_source_file = b.path("src/client/root.zig"),
+            .imports = &.{
+                .{ .name = "wayland-protocols", .module = wayland_protocols },
+            },
+        });
+
+        return .{
+            .wayland_protocols = wayland_protocols,
+            .wayland_client = wayland_client,
+        };
+    }
+
+    fn initSteps(b: *std.Build) BuildManagerSteps {
+        return .{
+            .check = b.step("check", "Check if examples compile"),
+            .run = b.step("run", "Run an example (set via `-Dexample`)"),
+        };
+    }
+
+    fn init(b: *std.Build) Self {
+        return .{
+            .b = b,
+            .options = initOptions(b),
+            .modules = initModules(b),
+            .steps = initSteps(b),
+        };
+    }
+
+    fn setupExamples(self: *const Self) !void {
+        const selected_example_name_option = self.b.option([]const u8, "example", "The name of the example to run");
+
+        var examples_dir = try std.fs.cwd().openDir("examples", .{ .iterate = true });
+        defer examples_dir.close();
+
+        var examples_dir_iterator = examples_dir.iterate();
+
+        while (try examples_dir_iterator.next()) |entry| {
+            if (entry.kind != .file) continue;
+
+            const example_name = entry.name[0..(entry.name.len - 4)];
+            const example_path = self.b.path(self.b.pathJoin(&.{ "examples", entry.name }));
+
+            const example = self.b.addExecutable(.{
+                .name = example_name,
+                .root_source_file = example_path,
+                .target = self.options.target,
+                .optimize = self.options.optimize,
+                .use_lld = self.options.use_llvm,
+                .use_llvm = self.options.use_llvm,
+                .strip = self.options.strip,
+            });
+            example.root_module.addImport("wayland-client", self.modules.wayland_client);
+            example.root_module.addImport("wayland-protocols", self.modules.wayland_protocols);
+
+            self.steps.check.dependOn(&example.step);
+
+            if (selected_example_name_option) |selected_example_name| {
+                if (std.mem.eql(u8, example_name, selected_example_name)) {
+                    const run_cmd = self.b.addRunArtifact(example);
+                    self.steps.run.dependOn(&run_cmd.step);
+                    if (self.b.args) |args| {
+                        run_cmd.addArgs(args);
+                    }
+                }
+            }
         }
     }
+};
+
+pub fn build(b: *std.Build) !void {
+    const buildManager = BuildManager.init(b);
+    try buildManager.setupExamples();
 }
