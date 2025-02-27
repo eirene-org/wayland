6163996402e91f3356c2bed74c42678e225838cb
Pavel Sobolev <contact@paveloom.dev>
2025-02-27 12:37:11 +0300

    Add names and versions to the interfaces.

--- src/protocols/wayland.zig
+++ src/protocols/wayland.zig
@@ -3,6 +3,9 @@ const wp = @import("root.zig");
 pub const wl_display = enum(wp.Word) {
     _,
 
+    pub const NAME = "wl_display";
+    pub const VERSION = 1;
+
     const Self = @This();
 
     pub const Request = union(enum(wp.Opcode)) {
@@ -19,20 +22,11 @@ pub const wl_display = enum(wp.Word) {
     };
 };
 
-pub const wl_callback = enum(wp.Word) {
-    _,
-
-    pub const Event = union(enum(wp.Opcode)) {
-        done: Event.Done,
-
-        pub const Done = struct {
-            callback_data: wp.UInt,
-        };
-    };
-};
-
 pub const wl_registry = enum(wp.Word) {
     _,
+
+    pub const NAME = "wl_registry";
+    pub const VERSION = 1;
 
     pub const Event = union(enum(wp.Opcode)) {
         global: Event.Global,
@@ -43,4 +37,33 @@ pub const wl_registry = enum(wp.Word) {
             version: wp.UInt,
         };
     };
+};
+
+pub const wl_callback = enum(wp.Word) {
+    _,
+
+    pub const NAME = "wl_callback";
+    pub const VERSION = 1;
+
+    pub const Event = union(enum(wp.Opcode)) {
+        done: Event.Done,
+
+        pub const Done = struct {
+            callback_data: wp.UInt,
+        };
+    };
+};
+
+pub const wl_compositor = enum(wp.Word) {
+    _,
+
+    pub const NAME = "wl_compositor";
+    pub const VERSION = 6;
+};
+
+pub const wl_shm = enum(wp.Word) {
+    _,
+
+    pub const NAME = "wl_shm";
+    pub const VERSION = 2;
 };
